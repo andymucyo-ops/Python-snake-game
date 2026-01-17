f771ab068d2eecfab2048a0f7b9ac1141add0e91
a-nkunzimana <andymucyo@gmail.com>
2026-01-17 18:43:22 +0100

    defined game class and added board rendering method

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+
+from .game import Game
+from .apple import Apple
+from .snake import Snake
+     
+__all__ = [Game,Apple,Snake]
--- src/game.py
+++ src/game.py
@@ -1,4 +1,30 @@
 
 class Game:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,height: int,width: int) -> None:
+        self.height = height
+        self.width = width
+
+    def render(self):
+        empty_board: list[list] = []
+
+        for line in range(self.height):
+
+            empty_line: list = []
+            
+            for col in range(self.width):
+                
+                empty_line.append(None)
+
+            empty_board.append(empty_line)    
+        
+        print(f'Hight: {self.height}')
+        print(f'Width: {self.width}')
+        return empty_board
+
+
+if __name__ == '__main__' :
+    game = Game(4,2)
+
+    game.render()
+
+    print(game.render())
