86850796f894c558ac8eb9f60218662727a0ae34
a-nkunzimana <andymucyo@gmail.com>
2026-01-19 17:35:18 +0100

    added direction constants form snake module

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 
 from .game import Game
 from .apple import Apple
-from .snake import Snake
+from .snake import Snake, UP, DOWN, LEFT, RIGHT 
      
-__all__ = [Game,Apple,Snake]
+__all__ = [Game, Apple, Snake, UP, DOWN, LEFT, RIGHT]
