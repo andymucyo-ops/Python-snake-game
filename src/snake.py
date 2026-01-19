f0f306e5da3367089fa8cf0de192a457727c71bd
a-nkunzimana <andymucyo@gmail.com>
2026-01-20 00:12:24 +0100

    fixed direction consstants for snake movement

--- src/snake.py
+++ src/snake.py
@@ -1,23 +1,30 @@
 # direction tuple[int]s
-UP: tuple[int] = (0,1)
-DOWN: tuple[int] = (0,-1)
-LEFT: tuple[int] = (-1,0)
-RIGHT: tuple[int] = (1,0) 
+RIGHT: tuple[int] = (0,1)
+LEFT: tuple[int] = (0,-1)
+UP: tuple[int] = (-1,0)
+DOWN: tuple[int] = (1,0) 
 
 #class definition
 class Snake:
     def __init__(self, body: list[tuple[int]], direction: tuple[int]) -> None:
         self.body = body
         self.direction = direction
-    
-    def __iter__(self):
-        return iter(self.body)
 
-    def move(self, position: tuple[int]) -> list[tuple[int]]:
-        self.body = self.body[1:].append(position) 
+    # def body(self) -> list[tuple[int]]:
+    #     return self.body()
+
+    def __iter__(self) -> None:
+        return iter(self.body)
     
-    def set_direciton(self, direction: tuple[int]) -> None:
-        self.direction = direction
+    def move(self, position: tuple[int]) -> list[tuple[int]]: 
+        self.body = self.body[1:]
+        
+        self.body.append(position) 
+
+        return self.body
+    
+    # def set_direciton(self)-> None:
+    #     self.direction = direction
 
     def head(self) -> tuple[int]:
 
