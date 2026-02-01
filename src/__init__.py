e3dc6d4e80029156f07da73b60c7777ca7f7cbc6
a-nkunzimana <andymucyo@gmail.com>
2026-02-01 21:09:57 +0100

    corrected __all__

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ from .game import Game
 from .apple import Apple
 from .snake import Snake, UP, DOWN, LEFT, RIGHT 
      
-__all__ = [Game, Apple, Snake, UP, DOWN, LEFT, RIGHT]
+__all__ = ["Game", "Apple", "Snake", "UP", "DOWN", "LEFT", "RIGHT"]
