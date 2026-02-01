a6513b2e969c1ba0d13ec71b2da22ddc0ba32acc
a-nkunzimana <andymucyo@gmail.com>
2026-02-01 21:14:51 +0100

    initialize game

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 
+#imports
+from pyfiglet import Figlet
+from os import system
+from src import game, snake, apple # noqa: F401
+
+#initial game print to welcome the player
+welcome_msg: Figlet = Figlet(font = "larry3d",)
+
+print(welcome_msg.renderText("TERMINAL SNAKE GAME"))
+
+input("Press any key to start")
+
+system("clear")
+
+
+# setup game board
+
+board = game.Game(16,32)
+
+# start game
+
+board.display_print()
+
+board.player_interaction()
+
